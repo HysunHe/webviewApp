9569611139601cd770439dcc8b259be82c63ce54
Hysun He <hysunhe@foxmail.com>
2020-01-03 19:15:23 +0800

    impl

---
+++ upgrade-wvapp.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+echo "Deleting old code..."
+rm -rf /home/oracle/hysun-workspace/webviewApp
+
+sleep 1
+
+echo "pull latest code"
+cd /home/oracle/hysun-workspace
+git clone https://github.com/HysunHe/webviewApp.git
+
+sleep 1
+
+echo "npm install webviewApp"
+cd /home/oracle/hysun-workspace/webviewApp/
+chmod +x *.sh
+npm install
+
+sleep 1
+
+echo "Producing new image..."
+./build-image.sh
+
+sleep 1
+
+echo "Starting odaqr..."
+
+./start.sh
+
+sleep 1
+
+echo "Done!"
+
+cd
